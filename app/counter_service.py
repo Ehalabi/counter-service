5678cd7049da6e3591307635b62102e41da25d46
eyad.halabi <ehalabi2@gmail.com>
2026-01-14 23:56:26 +0200

    add prometheus client into counter service code and requirements file

--- app/counter_service.py
+++ app/counter_service.py
@@ -2,6 +2,7 @@
 import os
 import redis
 from flask import Flask, request
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
@@ -10,10 +11,19 @@ r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 app = Flask(__name__)
 
+http_requests_total = COUNTER("http_requests_total", "Total number of HTTP requests received", ["status", "path", "method"])
+
+@app.after_request
+def after_request(response):
+    """Increment counter after each request"""
+    http_requests_total.labels(
+        status=str(response.status_code), path=request.path, method=request.method
+    ).inc()
+    return response
+
 @app.route("/metrics")
 def metrics():
-    count = r.get("counter") or 0
-    return f"request_counter_total {count}\n", 200, {'Content-Type': 'text/plain'}
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 @app.route('/healthz')
 def healthz():
